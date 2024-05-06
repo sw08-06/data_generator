49ac1182c1975bfc6e4b51fa4bb07b51c8d53323
lasse2507a <lran18@student.aau.dk>
2024-05-06 15:31:04 +0200

    prediction generator

--- src/main.py
+++ src/main.py
@@ -18,53 +18,53 @@ client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-class DataGenerator:
-    def __init__(self, start_day, days, window_size, stress_probability_dict):
+class PredictionGenerator:
+    def __init__(self, start_unix_time, days, window_size, stress_probability_dict, wear_time_dict):
         self.window_size = window_size
         self.days = days
-        self.start_day = start_day  # format: 1712181600 (4/4/24 00:00:00)
+        self.start_unix_time = start_unix_time  # format: 1712181600 (4/4/24 00:00:00)
+        self.end_unix_time = start_unix_time + days * 86400
         self.stress_probability_dict = stress_probability_dict
-
-        self.dates = []
-        for i in range(self.days):
-            self.dates.append(self.start_day + (86400 * i))  # 86400 = 1 day in seconds
+        self.wear_time_dict = wear_time_dict
+        self.predictions = []
 
     def generate_predictions(self):
-        for i in range(self.days):
-            wear_probability = self.wear_probability()
-            if wear_probability == "work_hours":
-                self.dates[i] += 28800  # Add 8 hours
-                for j in range(28800 / self.window_size):  # Predict from 8:00 - 16:00
-                    
-                    # add 1 minute
+        predictions = []
+        current_time = self.start_day
+        while current_time < self.end_day:
+            wear_mode = self.random_wear_mode()
+            current_time += self.wear_time_dict(wear_mode)
+            if not wear_mode == "not_wear":
+                for _ in range(wear_time_dict(wear_mode) / self.window_size):
+                    hour_interval = self.find_hour_interval(current_time)
+                    predictions.append(self.calculate_prediction(self.determine_weekend_or_week_day(current_time), hour_interval))
+                    current_time += self.window_size
+                if wear_mode == "work_hours":
+                    current_time += self.wear_time_dict(wear_mode)
+        self.predictions = predictions
 
-                    # elif(wear_probability == "all_day"):
-                    # for j in range(1440): # Predict from 00:00 - 24:00
-                    # generate prediction
-                    # add 1 minute
-                    continue
-            else:
-                continue
-
-    def stress_probability(self, timestamp):
-        hour = datetime.fromtimestamp(timestamp).hour
-        random_number = random.random()
-        
-
-    def wear_probability():
+    def random_wear_mode():
         random_number = random.random()
 
         if random_number < 0.6:
-            return "work_hours"
+            return "work_day"
         elif random_number < 0.8:
             return "all_day"
         else:
             return "not_wear"
 
-    def is_weekend(self, timestamp):
-        date = datetime.fromtimestamp(timestamp)
+    def find_hour_interval(self, current_time):
+        hour = datetime.fromtimestamp(current_time).hour
+        lower = 3 * (hour // 3)
+        upper = lower + 3
+        return f"{lower}-{upper}"
 
-        return date.weekday() in [5, 6]
+    def determine_weekend_or_week_day(self, timestamp):
+        date = datetime.fromtimestamp(timestamp)
+        return "weekend" if date.weekday() in [5, 6] else "week_day"
+
+    def calculate_prediction(self, weekend_or_weekday, hour_interval):
+        return 1 if random.random() < self.stress_probability_dict[weekend_or_weekday][hour_interval] else 0
 
 
 # def write_data(data):
@@ -74,7 +74,7 @@ class DataGenerator:
 
 # write_data(data)
 if __name__ == "__main__":
-    stress_probabilty_dict = {
+    stress_probability_dict = {
         "weekend": {
             "0-3": 0.02,
             "3-6": 0.02,
@@ -96,5 +96,6 @@ if __name__ == "__main__":
             "21-24": 0.05,
         },
     }
+    wear_time_dict = {"work_day": 28800, "all_day": 0, "not_wear": 86400}
 
-    dataGen = DataGenerator(start_day=1712181600, days=28, window_size=60, stress_probability_dict=stress_probabilty_dict)
+    dataGen = PredictionGenerator(start_day=1712181600, days=28, window_size=60, stress_probability_dict=stress_probability_dict, wear_time_dict=wear_time_dict)
