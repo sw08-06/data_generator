988906b523fa24f9fed962f6d7579da9bc9ae0ef
lasse2507a <lran18@student.aau.dk>
2024-05-07 09:06:19 +0200

    write data with influx

--- src/main.py
+++ src/main.py
@@ -18,42 +18,48 @@ client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-# def write_data(data):
-#   for data_points in data():
-#        write_api.write(bucket=bucket, org=org, record=data_points)
+def write_data_influxdb(data):
+    with influxdb_client.InfluxDBClient(url=url, token=token, org=org) as client:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket=bucket, org=org, record=data)
 
 
-# write_data(data)
+stress_probability_dict = {
+    "weekend": {
+        "0-3": 0.02,
+        "3-6": 0.02,
+        "6-9": 0.02,
+        "9-12": 0.1,
+        "12-15": 0.1,
+        "15-18": 0.1,
+        "18-21": 0.02,
+        "21-24": 0.02,
+    },
+    "weekday": {
+        "0-3": 0.05,
+        "3-6": 0.05,
+        "6-9": 0.5,
+        "9-12": 0.4,
+        "12-15": 0.3,
+        "15-18": 0.2,
+        "18-21": 0.1,
+        "21-24": 0.05,
+    },
+}
+wear_time_dict = {"work_day": 28800, "all_day": 0, "not_wear": 86400}
+dataGen = PredictionGenerator(start_day=1712181600, days=28, window_size=60, stress_probability_dict=stress_probability_dict, wear_time_dict=wear_time_dict)
+prediction_points = dataGen.generate_predictions()
 
-if __name__ == "__main__":
-    stress_probability_dict = {
-        "weekend": {
-            "0-3": 0.02,
-            "3-6": 0.02,
-            "6-9": 0.02,
-            "9-12": 0.1,
-            "12-15": 0.1,
-            "15-18": 0.1,
-            "18-21": 0.02,
-            "21-24": 0.02,
-        },
-        "weekday": {
-            "0-3": 0.05,
-            "3-6": 0.05,
-            "6-9": 0.5,
-            "9-12": 0.4,
-            "12-15": 0.3,
-            "15-18": 0.2,
-            "18-21": 0.1,
-            "21-24": 0.05,
-        },
-    }
-    wear_time_dict = {"work_day": 28800, "all_day": 0, "not_wear": 86400}
+write_data_influxdb(prediction_points)
 
-    dataGen = PredictionGenerator(start_day=1712181600, days=28, window_size=60, stress_probability_dict=stress_probability_dict, wear_time_dict=wear_time_dict)
+while True:
+    start_time = time.time()
 
     data_points = data_generator(60)
 
-    with influxdb_client.InfluxDBClient(url=url, token=token, org=org) as client:
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-        write_api.write(bucket=bucket, org=org, record=data_points)
+    write_data_influxdb(data_points)
+
+    elapsed_time = time.time() - start_time
+
+    if elapsed_time < 60:
+        time.sleep(60 - elapsed_time)
